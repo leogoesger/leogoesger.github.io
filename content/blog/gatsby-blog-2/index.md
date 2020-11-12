4daede1c6edb572d9f8291f9ed14323a65ee2edd
Leo Qiu <leoq91@gmail.com>
2020-11-12 08:15:09 -0800

    add styling

--- content/blog/gatsby-blog-2/index.md
+++ content/blog/gatsby-blog-2/index.md
@@ -1,6 +1,12 @@
 ---
-title: 'Build a Gatsby Blog Part II: Dark Theme'
+title: 'Build a Gatsby Blog Part II: Dark Theme, Meta Image and Scroll To Anchor'
 date: '2020-09-10T22:12:03.284Z'
 status: 'draft'
 tags: ["coding"]
 ---
+
+Dark Theme
+
+Meta Image
+
+Scroll To
--- src/components/tags.js
+++ src/components/tags.js
@@ -8,10 +8,10 @@ const TagsComponent = ({
     group
 }) => {
     return (
-        <ul style={{ margin: "1rem 0px 0px 0px", display: "flex", listStyleType: "none" }}>
+        <ul style={{ flexWrap: "wrap", display: "flex", listStyleType: "none", margin: "0.5rem 0 0 0" }}>
             {group.sort((a, b) => a.fieldValue.localeCompare(b.fieldValue)).map((tag, idx) => (
-                <Link key={tag.fieldValue} to={`/tags/${kebabCase(tag.fieldValue)}/`} style={{ textDecoration: "none", fontSize: 14, }}>
-                    <button style={{ marginRight: "0.3rem", border: "none", padding: "4px 6px", backgroundColor: "var(--buttonBackground)", width: 80, color: "var(--buttonText)", fontWeight: "bold", cursor: "pointer" }}>
+                <Link key={tag.fieldValue} to={`/tags/${kebabCase(tag.fieldValue)}/`} style={{ textDecoration: "none", fontSize: 14, marginBottom: "0.5rem", marginRight: "0.4rem" }}>
+                    <button style={{ border: "none", padding: "4px 6px", backgroundColor: "var(--buttonBackground)", width: 80, color: "var(--buttonText)", fontWeight: "bold", cursor: "pointer" }}>
                         {tag.fieldValue}
                     </button>
                 </Link>
